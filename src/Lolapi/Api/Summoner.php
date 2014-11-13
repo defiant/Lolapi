17823a7c64c3d20875f9012e86ebd74d4a7c0ae6
Sinan Taga <sinantaga@gmail.com>
2014-11-13 16:58:39 +0200

    Composite query method for summoners

--- src/Lolapi/Api/Summoner.php
+++ src/Lolapi/Api/Summoner.php
@@ -8,27 +8,59 @@ namespace Lolapi\Api;
  */
 class Summoner extends AbstractApi{
 
+    const MAXIMUM = 40;
     /**
      * Api version to query
      * @var string
      */
     protected $version = '1.4';
 
-    /**
-     * Api region to query
-     * @var string
-     */
-    protected $region = 'tr';
+    public function get($summoners)
+    {
+        $ids = $names = [];
 
+        if (is_array($summoners)) {
+            foreach($summoners as $summoner){
+                if (is_int($summoner)) {
+                    $ids[] = $summoner;
+                }else{
+                    $names[] = $summoner;
+                }
+            }
+        }else{
+            if (is_numeric($summoners)) {
+                $ids[] = $summoners;
+            }else{
+                $names [] = $summoners;
+            }
+        }
 
+        $summonerIds = $summonerNames = [];
+
+        if ($ids) {
+            $summonerIds = $this->byId($ids);
+        }
+
+        if ($names) {
+            $summonerNames = $this->byName($names);
+        }
+
+        return array_merge($summonerIds + $summonerNames);
+    }
+    
     /**
      * Get summoner objects mapped by standardized summoner name for a given list of summoner names. (REST)
      *
      * @param $ids
+     * @throws \MaxException
+     * @return Array json
      */
     public function byId($ids)
     {
         if (is_array($ids)) {
+            if (count($ids) > self::MAXIMUM) {
+                throw new \MaxException('You can query only ' . self::MAXIMUM . ' ids per request');
+            }
             $ids = implode(",", $ids);
         }
 
@@ -39,13 +71,16 @@ class Summoner extends AbstractApi{
      * Get summoner objects mapped by summoner ID for a given list of summoner IDs. (REST)
      *
      * @param $names
-     *
+     * @throws \MaxException
      * @return summoner info json
      */
     public function byName($names)
     {
 
         if (is_array($names)) {
+            if (count($names) > self::MAXIMUM) {
+                throw new \MaxException('You can query only ' . self::MAXIMUM . ' ids per request');
+            }
             $names = implode(",", $names);
         }
         return $this->call('summoner/by-name/'.$names);
---
+++ src/Lolapi/Exceptions/MaxException.php
@@ -0,0 +1,4 @@
+<?php
+class MaxException extends \Exception{
+
+} 
